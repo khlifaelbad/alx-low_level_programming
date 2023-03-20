03648fe164c4adde83ac01348a2860d293848778
khlifaelbad <khlifaelbad@gmail.com>
2023-03-19 20:08:57 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+/*
+ * File: 8-print_base16.c
+ * Auth: Khlifa elbad
+ */
+#include <stdio.h>
+/**
+ * main -  prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
