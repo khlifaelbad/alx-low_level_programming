5ddab5a87f0f32ff0b3f12092a63bff9e5676a3a
khlifaelbad <khlifaelbad@gmail.com>
2023-03-19 20:02:15 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9.
+ * main -   prints the alphabet in lowercase
  *
- * Return: Always 0 (Success)
- */
+ * Return: Always 0.
+*/
 int main(void)
 {
-	int i;
+	char letter;
 
-	for (i = 0; i < 10; i++)
+	for (letter = 'a' ; letter <= 'z' ; letter++)
 	{
-		printf("%d", i);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
-	putchar('\n');
-	return (0);
-}
 
+	putchar('\n');
+
+	return (0);
+
+}
