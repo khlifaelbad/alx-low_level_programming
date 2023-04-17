ce77543726359e79ca6bf05a8a0eba4f4853f45e
khlifaelbad <khlifaelbad@gmail.com>
2023-04-17 09:19:06 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("%s\n", __FILE__);  // Print the name of the current file
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
+
