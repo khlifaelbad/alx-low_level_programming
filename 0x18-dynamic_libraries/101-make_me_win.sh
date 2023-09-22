bc19c1886fc88dcdf440b81f9edd3e8064b5600c
khlifaelbad <khlifaelbad@gmail.com>
2023-09-22 13:46:15 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -N -P /tmp https://raw.githubusercontent.com/khlifaelbad/alx-low_level_programming/master/0x18-dynamic_libraries/randomx.so
-export LD_PRELOAD="/tmp/randomx.so"
+wget -P /tmp https://github.com/khlifaelbad/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
+
