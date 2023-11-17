03cc5e1866c76dad98885bd11f6ad1c1d937994b
khlifaelbad <khlifaelbad@gmail.com>
2023-11-17 14:32:33 -0800

    Update_0x18-dynamic_libraries

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -11,3 +12,4 @@ int main(void)
     printf("%d\n", _strlen("My Dyn Lib"));
     return (EXIT_SUCCESS);
 }
+
