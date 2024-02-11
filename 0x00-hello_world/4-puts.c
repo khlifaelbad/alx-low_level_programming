a4c6f7cf55f11e2c1d668b84ee9825d60f144c37
khlifaelbad <khlifaelbad@gmail.com>
2024-02-11 18:22:23 +0100

    function put(): hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point and use a function puts().
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
