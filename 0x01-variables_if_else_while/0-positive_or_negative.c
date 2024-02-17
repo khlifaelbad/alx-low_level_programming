699b3b97be0f91397ee38a274500378c2545b332
khlifaelbad <khlifaelbad@gmail.com>
2024-02-17 13:11:23 +0100

    Positive or Negativethen zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+
 /**
 * main - main block
 * Description: Get a random number and print the number
@@ -10,19 +11,18 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-    
         if (n > 0)
 		printf("%d is positive\n", n);
 
         else if (n < 0) 
 		printf("%d is negative\n", n);
   
-        else
+        else if (n == 0)
                 printf("%d is zero\n", n);
  
-    return (0);    
+        return (0);    
 }
 
