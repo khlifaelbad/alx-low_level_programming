7b4a6bcd01008aae68306facd51103c095853970
khlifaelbad <khlifaelbad@gmail.com>
2024-02-23 01:07:09 +0100

    prints all possible different combinations of two digits => putchar()

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,15 @@ int main(void)
 int num1, num2;
 
 for (num1 = 0; num1 < 9; num1++)
-	{
-	for (num2 = num1 + 1; num2 < 10; num2++)
-	{
-	putchar(num1 + '0');
-	putchar(num2 + '0');
-	if (num1 == 8 && num2 == 9)
-	continue;
-	putchar(',');
-	putchar(' '); 
-	}
-	}
+{
+for (num2 = num1 + 1; num2 < 10; num2++)
+putchar(num1 + '0');
+putchar(num2 + '0');
+if (num1 == 8 && num2 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
 putchar('\n');
 return (0);
 }
