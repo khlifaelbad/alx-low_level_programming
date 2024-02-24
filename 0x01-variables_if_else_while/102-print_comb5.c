c20085e09b0d61db0b3692ef82e6bffaa3196155
khlifaelbad <khlifaelbad@gmail.com>
2024-02-25 00:26:05 +0100

    prints all possible combinations of two numbers => putchar() integer exracted to ASCII

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
+
 /**
- * main -   print the last digit of the number stored in the variable n
- *
- * Return: Always 0.
-*/
+ * main - Entry point.
+ * Description: prints all possible combinations of two two-digit numbers.
+ * Return: Always 0 (Success).
+ */
+
 int main(void)
 {
-	int num1, num2;
+int num1, num2;
 
-	for (num1 = 0; num1 <= 98; num1++)
-	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
-		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 98 && num2 == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
-
-	return (0)
+for (num1 = 0; num1 < 99; num1++)
+{
+for (num2 = num1 + 1; num2 <= 99; num2++)
+{
+putchar(num1 / 10 + '0');
+putchar(num1 % 10 + '0');
+putchar(' ');
+putchar(num2 / 10 + '0');
+putchar(num2 % 10 + '0');
+if (num1 == 98 && num2 == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
