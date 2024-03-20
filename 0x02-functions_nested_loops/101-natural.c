8d13b7b9230a71f77da0eba95835841d8611dcc6
khlifaelbad <khlifaelbad@gmail.com>
2024-03-20 15:08:40 +0000

    prints the sum of all natural nums multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - list all the natural numbers below 10 that are multiples of 3 or 5
+ *  we get 3, 5, 6 and 9. The sum of these multiples is 23
+ *  and prints the sum of all the multiples of 3 or 5 below 1024.
+ * Return: Always with 0 (Success).
+ */
+
+int main(void)
+{
+	int number, sum = 0;
+	
+	for (number = 0; number < 1024; number++)
+	{
+		if ((number % 3 == 0) || (number % 5 == 0))
+			sum += number;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
