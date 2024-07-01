2ea999be8c2dcf0228649283a5a65188415ad0aa
khlifaelbad <khlifaelbad@gmail.com>
2024-07-01 17:57:52 +0100

    print a number by call a func

--- 101-print_number.c
+++ 101-print_number.c
@@ -5,24 +5,21 @@
  * @n: its a parameter of func to be print integer.
  * Return: nothing to return.
  */
+
  void print_number(int n)
  {
-    /* we have an argument with return data type void*/
-    /* print numbers by use _putchar*/
-
     unsigned int num;
     num = n;
 
     /*will check if numer is negative*/
-    if (n < 0) 
+    if (n < 0)
     {
-        _putchar('-'); /* print (-) if i get negative num */
-         num = -n; /* then will negative number */
+	    _putchar('-'); /*print (-) if i get negative num then will negative number*/
+	    num = -n;
     }
-    if (num / 10 > 0) /* let make an integer divise on 10 we get all nums left and end last no*/
+    if (num / 10 > 0) /*let make an integer divise on 10 we get all nums left and end last no*/
     {
-        print_number(num / 10); /* here we call function to print us whole number at stop to end num right */
+	    print_number(num / 10); /* here we call function to print us whole number at stop to end num right */
     }
-    _putchar((num % 10) + '0'); /* then well print each num by a num to end right number bu use num % 10 
-    for example 4 + 48 = 52 << ASCII convert 52 as decimal to see number */
+    _putchar((num % 10) + '0'); /*then well print each num by a num to end right number bu use num % 10 for example 4 + 48 = 52 << ASCII convert 52 as decimal to see number*/
  }
