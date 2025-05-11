328f2ed760b0963eca0ff6127ae52801a646a531
Khlifa elbad <khlifaelbad@gmail.com>
2025-05-12 00:58:24 +0100

    Add recursive function to compute natural square root
    - Implemented _sqrt_recursion(int n) to return the natural square root of a number.
    - Returns -1 if the number does not have a natural square root or is negative.
    - Uses helper recursion with increasing guess value i.
    - Avoids static variables and loops to comply with constraints.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,18 +4,19 @@
  * _sqrt_recursion - Its a function that return the natural square root of num
  * @n: Its a natural square to be printed.
  * Return: the function by a number n to find square root and x the current.
- *
- * _sqrt_helper - Its a func find the square root recursively
- *
- * @n: Its number to find the square root.
- * @x: The current guess for the square root.
- * Return: The square root, or -(1) if no natural number square root exist
  */
 int _sqrt_recursion(int n)
 {
 	return (_sqrt_helper(n, 1));
 }
 
+ /**
+ * _sqrt_helper - Its a func find the square root recursively
+ *
+ * @n: Its number to find the square root.
+ * @x: The current guess for the square root.
+ * Return: The square root, or -(1) if no natural number square root exist
+ */
 int _sqrt_helper(int n, int x)
 {
 	if (x * x == n)
