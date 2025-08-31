c5d484f35f11715bb9f12cbf7b65a74d77f2e939
khlifaelbad <khlifaelbad@gmail.com>
2025-08-31 02:33:21 +0100

    update and fixed issue

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * ft_strlen - that a func count length of string.
+ *
+ * @s: its a string to be counted.
+ *
+ * Return: return a length of string.
+ */
+int	ft_strlen(char *s)
+{
+	if(*s == '\0')
+		return (0);
+	return (1 + ft_strlen(s + 1));
+}
+/**
  * is_palindrome_helper - recursive func that compares two char of string.
  *
  * @s: pointer to the string being checked.
@@ -12,13 +25,10 @@
  */
 int	is_palindrome_helper(char *s, int i, int j)
 {
-	/* check if string is empty so palindrome */
-	if (*(s + i) == '\0')
-		return (1);
 	if (i >= j)
 		return (1);
 	/*compare first charcter to the last character*/
-	if (*(s + i) != *(s + j - 1))
+	if (*(s + i) != *(s + j))
 		return (0);
 	return (is_palindrome_helper(s, i + 1, j - 1));
 }
@@ -35,7 +45,7 @@ int	is_palindrome(char *s)
 
 	j = 0;
 	if (*s == '\0')
-		return (0);
-	j = 1 + is_palindrome(s + 1);
-	return (is_palindrome_helper(s, 0, j));
+		return (1);
+	j = ft_strlen(s);
+	return (is_palindrome_helper(s, 0, j - 1));
 }
