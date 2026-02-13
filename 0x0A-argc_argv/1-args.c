8184634eb3145a5deb41955ecfbae7e64eb0ab80
khlifaelbad <khlifaelbad@gmail.com>
2026-02-13 18:05:06 +0100

    Task 01: Write a program that prints the number of arguments passed into it.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdio.h>
 
-void print_nums(long num);
+/*void print_nums(long num);*/
 /**
  * main - a program that prints number arguments
  *
@@ -13,22 +14,17 @@ void print_nums(long num);
 int main(int argc, char *argv[])
 {
 	(void) *argv;
-	print_nums(argc - 1);
-	write(1, "\n", 1);
+
+	printf("%d\n", argc - 1);
+	/*print_nums(argc - 1);*/
+	/*write(1, "\n", 1);*/
 	return (0);
 }
-/**
- * print_nums - that a func prints all digits
- *
- * @num: its a number to be printed
- *
- * return: Nothing to return
- */
-void print_nums(long num)
-{
-	if (num > 9)
-	{
-		print_nums(num / 10);
-	}
-	_putchar(num % 10 + '0');
-}
+/*void print_nums(long num)*/
+/*{*/
+	/*if (num > 9)*/
+	/*{*/
+		/*print_nums(num / 10);*/
+	/*}*/
+	/*_putchar(num % 10 + '0');*/
+/*}*/
